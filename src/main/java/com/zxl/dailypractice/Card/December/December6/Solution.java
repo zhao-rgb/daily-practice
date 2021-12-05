4662dbda1472653099097762aa68f45703a370fe
zhao-rgb <1019916061@qq.com>
2021-12-05 21:01:31 +0800

    超级次方提交

---
+++ src/main/java/com/zxl/dailypractice/Card/December/December6/Solution.java
@@ -0,0 +1,11 @@
+package com.zxl.dailypractice.Card.December.December6;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2021/12/5
+ * @Version 1.0
+ **/
+public class Solution {
+}
