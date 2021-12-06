bcd7351b236d1f46a9185f2f673b9d59f5b6dbaf
zhao-rgb <1019916061@qq.com>
2021-12-06 20:50:15 +0800

    设计模式

---
+++ src/main/java/com/zxl/dailypractice/DesignPattern/DecoratorPattern/RedShapeDecorator.java
@@ -0,0 +1,26 @@
+package com.zxl.dailypractice.DesignPattern.DecoratorPattern;
+
+/**
+ * @author zhao
+ * @className RedShapeDecorator
+ * @Description TODO
+ * @Date 2021/12/6
+ * @Version 1.0
+ **/
+public class RedShapeDecorator extends ShapeDecorator{
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw(){
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+
+    private void setRedBorder(Shape decoratedShape){
+        System.out.println("Border Color: Red");
+    }
+
+}
---
+++ src/main/java/com/zxl/dailypractice/DesignPattern/DecoratorPattern/Shape.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.DesignPattern.DecoratorPattern;
+
+/**
+ * @author zhao
+ * @className Shape
+ * @Description TODO
+ * @Date 2021/12/6
+ * @Version 1.0
+ **/
+public interface Shape {
+    void draw();
+}
