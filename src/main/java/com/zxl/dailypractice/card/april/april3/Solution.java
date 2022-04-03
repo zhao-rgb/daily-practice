5ed10785d9ded8b199f3adc66f424142b0769549
zhao-rgb <1019916061@qq.com>
2022-04-03 15:25:26 +0800

    java基础4.3

---
+++ src/main/java/com/zxl/dailypractice/card/april/april3/Solution.java
@@ -0,0 +1,30 @@
+package com.zxl.dailypractice.card.april.april3;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2022/4/3
+ * @Version 1.0
+ **/
+// 寻找比目标字母大的最小字母      https://leetcode-cn.com/problems/find-smallest-letter-greater-than-target/
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char nextGreater = letters[0];
+        for (char letter : letters) {
+            if (letter > target) {
+                nextGreater = letter;
+                break;
+            }
+        }
+        return nextGreater;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] a ={'c','f','j'};
+        char b = 'a';
+        char c = solution.nextGreatestLetter(a, b);
+        System.out.println(c);
+    }
+}
