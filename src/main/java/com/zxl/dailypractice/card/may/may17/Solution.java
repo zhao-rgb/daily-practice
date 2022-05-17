f153031af2141eda1e12b4ad1ca6ea45b9c94d10
zhao-rgb <1019916061@qq.com>
2022-05-17 12:24:40 +0800

    java基础5.17

---
+++ src/main/java/com/zxl/dailypractice/card/may/may17/Solution.java
@@ -0,0 +1,38 @@
+package com.zxl.dailypractice.card.may.may17;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2022/5/17
+ * @Version 1.0
+ **/
+//验证外星语词典      https://leetcode.cn/problems/verifying-an-alien-dictionary/
+public class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] index = new int[26];
+        for (int i = 0; i < order.length(); ++i) {
+            index[order.charAt(i) - 'a'] = i;
+        }
+        for (int i = 1; i < words.length; i++) {
+            boolean valid = false;
+            for (int j = 0; j < words[i - 1].length() && j < words[i].length(); j++) {
+                int prev = index[words[i - 1].charAt(j) - 'a'];
+                int curr = index[words[i].charAt(j) - 'a'];
+                if (prev < curr) {
+                    valid = true;
+                    break;
+                } else if (prev > curr) {
+                    return false;
+                }
+            }
+            if (!valid) {
+                /* 比较两个字符串的长度 */
+                if (words[i - 1].length() > words[i].length()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
