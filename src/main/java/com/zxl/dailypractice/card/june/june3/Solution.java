5d8569b2682616c0e867a8a2a129bba93aca97c5
zhao-rgb <1019916061@qq.com>
2022-06-03 08:18:11 +0800

    java基础6.3

---
+++ src/main/java/com/zxl/dailypractice/card/june/june3/Solution.java
@@ -0,0 +1,30 @@
+package com.zxl.dailypractice.card.june.june3;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2022/6/3
+ * @Version 1.0
+ **/
+//连续整数求和   https://leetcode.cn/problems/consecutive-numbers-sum/
+public class Solution {
+    public int consecutiveNumbersSum(int n) {
+        int ans = 0;
+        int bound = 2 * n;
+        for (int k = 1; k * (k + 1) <= bound; k++) {
+            if (isKConsecutive(n, k)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isKConsecutive(int n, int k) {
+        if (k % 2 == 1) {
+            return n % k == 0;
+        } else {
+            return n % k != 0 && 2 * n % k == 0;
+        }
+    }
+}
