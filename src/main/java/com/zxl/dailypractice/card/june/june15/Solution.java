289c473c57cc4f215b2663fb8503614dea8a76ec
zhao-rgb <1019916061@qq.com>
2022-06-15 12:47:49 +0800

    java基础6.15

---
+++ src/main/java/com/zxl/dailypractice/card/june/june15/Solution.java
@@ -0,0 +1,34 @@
+package com.zxl.dailypractice.card.june.june15;
+
+import java.util.Arrays;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2022/6/15
+ * @Version 1.0
+ **/
+//找出第 K 小的数对距离   https://leetcode.cn/problems/find-k-th-smallest-pair-distance/
+public class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length, left = 0, right = nums[n - 1] - nums[0];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int cnt = 0;
+            for (int i = 0, j = 0; j < n; j++) {
+                while (nums[j] - nums[i] > mid) {
+                    i++;
+                }
+                cnt += j - i;
+            }
+            if (cnt >= k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
