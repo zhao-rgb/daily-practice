8459ddb08d58936684042c26a67afd8f267acd30
zhao-rgb <1019916061@qq.com>
2022-07-11 14:33:12 +0800

    java基础7.11

---
+++ src/main/java/com/zxl/dailypractice/card/july/july11/Solution.java
@@ -0,0 +1,43 @@
+package com.zxl.dailypractice.card.july.july11;
+
+/**
+ * @author zhao
+ * @className Solution
+ * @Description TODO
+ * @Date 2022/7/11
+ * @Version 1.0
+ **/
+//实现一个魔法字典    https://leetcode.cn/problems/implement-magic-dictionary/
+public class Solution {
+    private String[] words;
+
+    public Solution() {
+
+    }
+
+    public void buildDict(String[] dictionary) {
+        words = dictionary;
+    }
+
+    public boolean search(String searchWord) {
+        for (String word : words) {
+            if (word.length() != searchWord.length()) {
+                continue;
+            }
+
+            int diff = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                if (word.charAt(i) != searchWord.charAt(i)) {
+                    ++diff;
+                    if (diff > 1) {
+                        break;
+                    }
+                }
+            }
+            if (diff == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
