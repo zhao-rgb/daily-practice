0dcbc99c8c7c6d713b7ab6716c94ec01c47f0adc
zhao-rgb <1019916061@qq.com>
2022-07-25 09:24:18 +0800

    java基础7.25

---
+++ src/main/java/com/zxl/dailypractice/card/july/july25/Solution.java
@@ -0,0 +1,26 @@
+package com.zxl.dailypractice.card.july.july25;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/7/25
+ * @desc:
+ **/
+//公交站间的距离       https://leetcode.cn/problems/distance-between-bus-stops/
+public class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if (i >= start && i < destination) {
+                sum1 += distance[i];
+            } else {
+                sum2 += distance[i];
+            }
+        }
+        return Math.min(sum1, sum2);
+    }
+}
