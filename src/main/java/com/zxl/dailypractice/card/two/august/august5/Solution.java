9c171dac43f0b1ce72859b5d61af9f8d01335d72
zhao-rgb <1019916061@qq.com>
2022-08-05 09:31:33 +0800

    java基础8.5

---
+++ src/main/java/com/zxl/dailypractice/card/two/august/august5/Solution.java
@@ -0,0 +1,38 @@
+package com.zxl.dailypractice.card.two.august.august5;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/8/5
+ * @desc:
+ **/
+//在二叉树中增加一行      https://leetcode.cn/problems/add-one-row-to-tree/
+public class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
