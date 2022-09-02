0633e8acbf43e5833e2ed129c22c2a5c85839598
zhao-rgb <1019916061@qq.com>
2022-09-02 09:30:32 +0800

    java基础9.2

---
+++ src/main/java/com/zxl/dailypractice/card/two/September/September2/Solution.java
@@ -0,0 +1,14 @@
+package com.zxl.dailypractice.card.two.September.September2;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/9/1
+ * @desc:
+ **/
+public class Solution {
+    public int longestUnivaluePath(TreeNode root) {
+        return 1;
+    }
+}
