c172dca74617fe5eae0279b8890393ece65fc0ff
zhao-rgb <1019916061@qq.com>
2022-09-26 10:05:08 +0800

    java基础9.26

---
+++ src/main/java/com/zxl/dailypractice/card/two/September/September26/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.two.September.September26;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/9/26
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
