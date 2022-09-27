2a9106c02b644f4989f0f919417e3c06ca9f27cc
zhao-rgb <1019916061@qq.com>
2022-09-27 08:56:33 +0800

    java基础9.27

--- src/main/java/com/zxl/dailypractice/card/two/September/September26/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/September/September26/Solution.java
@@ -1,5 +1,9 @@
 package com.zxl.dailypractice.card.two.September.September26;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author: zhaoxl
  * @date: 2022/9/26
@@ -7,6 +11,27 @@ package com.zxl.dailypractice.card.two.September.September26;
  **/
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        String alaid ="2,3,4";
+        String[] ala =alaid.split(",");
+        StringBuffer a=new StringBuffer();
+        for(String id:ala){
+            a.append("'").append(id).append("'").append(",");
+        }
+        String typeid=a.deleteCharAt(a.length() - 1).toString();
+        System.out.println(typeid);
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String index = sdf.format(date);
+
+        String alaSql = String.format("select k_RESID from pm_col_flux_%s "
+                + "where k_NET = 'IPNET' and l_ALARMSTATUS = 1 and k_ALARMTYPEID in ("+typeid+") group by k_RESID limit 10000",index);
+        System.out.println(alaSql);
+
+        int alaNum = 10,tolNum = 30;
+        float ratio =(float)alaNum/tolNum*100;
+        DecimalFormat d=new DecimalFormat("0.00");
+        String format = d.format(ratio);
+        System.out.println(format);
     }
 }
