82b531d681fccb7367654637813f30a973e41e33
zhao-rgb <1019916061@qq.com>
2022-10-12 09:52:43 +0800

    java基础10.12-1

--- src/main/java/com/zxl/dailypractice/card/two/october/october11/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/october/october11/Solution.java
@@ -8,19 +8,6 @@ package com.zxl.dailypractice.card.two.october.october11;
 //仅执行一次字符串交换能否使两个字符串相等      https://leetcode.cn/problems/check-if-one-string-swap-can-make-strings-equal/
 public class Solution {
     public static void main(String[] args) {
-//        Student zxl = new Student("1", "zxl");
-//        Student zxl2 = new Student("2", "zxl2");
-//        Student zxl3 = new Student("3", "zxl3");
-//        Student zxl4 = new Student("4", "zxl4");
-//        List<Student> list = new ArrayList<>();
-//        list.add(zxl);
-//        list.add(zxl2);
-//        list.add(zxl3);
-//        list.add(zxl4);
-//        List<Student> collect = list.stream().filter(p -> "2".equals(p.id)).collect(Collectors.toList());
-//        System.out.println(collect);
-//        List<Student> collect1 = list.stream().parallel().collect(Collectors.toList());
-//        System.out.println(collect1);
         Solution solution = new Solution();
         boolean b = solution.areAlmostEqual("bank", "kand");
         System.out.println(b);
--- src/main/java/com/zxl/dailypractice/card/two/october/october12/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/october/october12/Solution.java
@@ -1,9 +1,31 @@
 package com.zxl.dailypractice.card.two.october.october12;
 
+
+import com.zxl.dailypractice.collection.ArrayList.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author: zhaoxl
  * @date: 2022/10/11
  * @desc:
  **/
 public class Solution {
+    public static void main(String[] args) {
+        Student zxl = new Student("1", "zxl");
+        Student zxl2 = new Student("2", "zxl2");
+        Student zxl3 = new Student("3", "zxl3");
+        Student zxl4 = new Student("4", "zxl4");
+        List<Student> list = new ArrayList<>();
+        list.add(zxl);
+        list.add(zxl2);
+        list.add(zxl3);
+        list.add(zxl4);
+        List<Student> collect = list.stream().filter(p -> "2".equals(p.id)).collect(Collectors.toList());
+        System.out.println(collect);
+        List<Student> collect1 = list.stream().parallel().collect(Collectors.toList());
+        System.out.println(collect1);
+    }
 }
