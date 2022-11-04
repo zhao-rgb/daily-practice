07fbefc2d547fa8ff9ac15488a3c11bf54cc4eb4
zhao-rgb <1019916061@qq.com>
2022-11-04 09:33:45 +0800

    java基础11.4

---
+++ src/main/java/com/zxl/dailypractice/card/two/november/november4/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.two.november.november4;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/11/3
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("usr.home", "/slview"));
+    }
+}
