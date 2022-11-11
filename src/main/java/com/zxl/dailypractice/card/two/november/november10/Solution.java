7ad862bc10afde97ea4bd838a9da0bd145b2ef11
zhao-rgb <1019916061@qq.com>
2022-11-11 09:33:01 +0800

    java基础11.11

--- src/main/java/com/zxl/dailypractice/card/two/november/november10/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/november/november10/Solution.java
@@ -1,7 +1,7 @@
 package com.zxl.dailypractice.card.two.november.november10;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 /**
  * @author: zhaoxl
@@ -10,23 +10,23 @@ import java.util.Date;
  **/
 public class Solution {
     public static void main(String[] args) {
-//        List<Map<String, Object>> resultMapList = new ArrayList<Map<String, Object>>();
-//        Map<String, Object> dataMap = new HashMap<String, Object>();
-//        String colenname = "olt_devip,accessnum,olt_devname,bas_devname,bas_devip,bas_portname,vlanid,bandwidth,ipv4,ipv4_mask,ipv4_buss,ipv6,ipv6_mask,ipv6_buss";
-//        String[] split = colenname.split(",");
-//        String colcnname = "Olt设备IP,1,专线接入号,Olt设备名称,Bas/SR设备名称,Bas/SR设备IP,Bas/SR端口名称,带宽,Ipv4地址,Ipv4掩码,Ipv4业务地址,Ipv6地址,Ipv6掩码,Ipv6业务地址";
-//        String[] split1 = colcnname.split(",");
-//        if(split.length == split1.length){
-//            for (int i = 0; i < split.length; i++) {
-//                dataMap = new HashMap<String, Object>();
-//                dataMap.put("colenname", split[i]);
-//                dataMap.put("colcnname", split1[i]);
-//                resultMapList.add(dataMap);
-//            }
-//        }else {
-//            System.out.println("列表对应不了");
-//        }
-//        System.out.println(resultMapList);
+        List<Map<String, Object>> resultMapList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        String colenname = "olt_devip,accessnum,olt_devname,bas_devname,bas_devip,bas_portname,vlanid,bandwidth,ipv4,ipv4_mask,ipv4_buss,ipv6,ipv6_mask,ipv6_buss";
+        String[] split = colenname.split(",");
+        String colcnname = "Olt设备IP,1,专线接入号,Olt设备名称,Bas/SR设备名称,Bas/SR设备IP,Bas/SR端口名称,带宽,Ipv4地址,Ipv4掩码,Ipv4业务地址,Ipv6地址,Ipv6掩码,Ipv6业务地址";
+        String[] split1 = colcnname.split(",");
+        if(split.length == split1.length){
+            for (int i = 0; i < split.length; i++) {
+                dataMap = new HashMap<String, Object>();
+                dataMap.put("colenname", split[i]);
+                dataMap.put("colcnname", split1[i]);
+                resultMapList.add(dataMap);
+            }
+        }else {
+            System.out.println("列表对应不了");
+        }
+        System.out.println(resultMapList);
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String format = sdf.format(new Date());
---
+++ src/main/java/com/zxl/dailypractice/card/two/november/november11/Solution.java
@@ -0,0 +1,27 @@
+package com.zxl.dailypractice.card.two.november.november11;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/11/11
+ * @desc:
+ **/
+//判断字符串的两半是否相似     https://leetcode.cn/problems/determine-if-string-halves-are-alike/
+public class Solution {
+    public boolean halvesAreAlike(String s) {
+        String a = s.substring(0, s.length() / 2);
+        String b = s.substring(s.length() / 2);
+        String h = "aeiouAEIOU";
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (h.indexOf(a.charAt(i)) >= 0) {
+                sum1++;
+            }
+        }
+        for (int i = 0; i < b.length(); i++) {
+            if (h.indexOf(b.charAt(i)) >= 0) {
+                sum2++;
+            }
+        }
+        return sum1 == sum2;
+    }
+}
