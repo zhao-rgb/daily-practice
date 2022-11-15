0f4f0507b0fb4ecf34ba76b4be1365cc9153cae9
zhao-rgb <1019916061@qq.com>
2022-11-15 15:21:27 +0800

    java基础11.15

---
+++ src/main/java/com/zxl/dailypractice/card/two/november/november15/Solution.java
@@ -0,0 +1,28 @@
+package com.zxl.dailypractice.card.two.november.november15;
+
+import java.util.Arrays;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/11/15
+ * @desc:
+ **/
+//卡车上的最大单元数   https://leetcode.cn/problems/maximum-units-on-a-truck/
+public class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        int res = 0;
+        for (int[] boxType : boxTypes) {
+            int numberOfBoxes = boxType[0];
+            int numberOfUnitsPerBox = boxType[1];
+            if (numberOfBoxes < truckSize) {
+                res += numberOfBoxes * numberOfUnitsPerBox;
+                truckSize -= numberOfBoxes;
+            } else {
+                res += truckSize * numberOfUnitsPerBox;
+                break;
+            }
+        }
+        return res;
+    }
+}
