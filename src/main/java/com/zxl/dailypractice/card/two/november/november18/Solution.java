45438098e9ee8ce6d2c4a293ca7fb33d6fa9cd3e
zhao-rgb <1019916061@qq.com>
2022-11-23 10:09:14 +0800

    java基础11.23

--- src/main/java/com/zxl/dailypractice/card/two/november/november18/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/november/november18/Solution.java
@@ -1,5 +1,9 @@
 package com.zxl.dailypractice.card.two.november.november18;
 
+import com.zxl.dailypractice.project.controller.req.GetsubTaskListReq;
+
+import java.lang.reflect.Field;
+
 /**
  * @author: zhaoxl
  * @date: 2022/11/18
@@ -7,14 +11,20 @@ package com.zxl.dailypractice.card.two.november.november18;
  **/
 public class Solution {
 
-    public static void main(String[] args) {
-        String a = "zlx";
-        for (int i = 0; i < 10; i++) {
-            if (i == 3){
-                System.out.println(i);
-                break;
-            }
+    public static void main(String[] args) throws IllegalAccessException {
+        GetsubTaskListReq getsubTaskListReq1 = new GetsubTaskListReq();
+        getsubTaskListReq1.setAccessnum("12s2");
+        Field[] fields = getsubTaskListReq1.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            //获取字段名称
+            String name = field.getName();
+            System.out.println(name);
+            Object value = null;
+            value = field.get(getsubTaskListReq1);
+            System.out.println(value);
         }
+        System.out.println(new GetsubTaskListReq().toString());
     }
 
 }
