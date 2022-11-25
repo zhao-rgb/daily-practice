86319e01944ea3f27523bca192f7aae26ec58ee0
zhao-rgb <1019916061@qq.com>
2022-11-25 09:30:46 +0800

    java基础11.25

--- src/main/java/com/zxl/dailypractice/card/two/november/november18/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/november/november18/Solution.java
@@ -1,8 +1,7 @@
 package com.zxl.dailypractice.card.two.november.november18;
 
-import com.zxl.dailypractice.project.controller.req.GetsubTaskListReq;
-
-import java.lang.reflect.Field;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author: zhaoxl
@@ -11,21 +10,57 @@ import java.lang.reflect.Field;
  **/
 public class Solution {
 
+    public static final String DIA_PRE_CFG_CHECK_WITH_PASSWORD = "{" +
+            "     <BODY>" +
+            "   ," +
+            "     \"DeviceIP\": \"<DeviceIP>\"," +
+            " }";
+
     public static void main(String[] args) throws IllegalAccessException {
-        GetsubTaskListReq getsubTaskListReq1 = new GetsubTaskListReq();
-        getsubTaskListReq1.setAccessnum("12s2");
-        Field[] fields = getsubTaskListReq1.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            //获取字段名称
-            String name = field.getName();
-            System.out.println(name);
-            Object value = null;
-            value = field.get(getsubTaskListReq1);
-            System.out.println(value);
+//        GetsubTaskListReq getsubTaskListReq1 = new GetsubTaskListReq();
+//        getsubTaskListReq1.setAccessnum("12s2");
+//        Field[] fields = getsubTaskListReq1.getClass().getDeclaredFields();
+//        for (Field field : fields) {
+//            field.setAccessible(true);
+//            //获取字段名称
+//            String name = field.getName();
+//            System.out.println(name);
+//            Object value = null;
+//            value = field.get(getsubTaskListReq1);
+//            System.out.println(value);
+//        }
+//        System.out.println(new GetsubTaskListReq().toString());
+//        //todo
+
+        String jsonParam = DIA_PRE_CFG_CHECK_WITH_PASSWORD
+                .replace("<BODY>", getCfgdeployBodyz())
+                .replace("<DeviceIP>", "s2ksk2");
+        System.out.println(jsonParam);
+    }
+
+
+    private static String getCfgdeployBodyz() {
+        String paradef = "{\"interface\":{\"type\":\"string\",\"description\":\"互联接口\",\"required\":true,\"value\":\"$11\"},\"aggreInterface\":{\"type\":\"string\",\"description\":\"聚合接口\",\"required\":true,\"value\":\"11\"}}";
+//        String paradef = "{\"port\":{\"type\":\"string\",\"description\":\"端口名称\",\"required\":true,\"value\":\"11\"}}";
+        System.out.println(paradef);
+        JSONObject jsonObject = JSONObject.parseObject(paradef);
+        System.out.println(jsonObject);
+        String body = "";
+        for (String key : jsonObject.keySet()) {
+            JSONObject childObject = jsonObject.getJSONObject(key);
+            String value = childObject.getString("value");
+            if (value.contains("$")){
+                value = value.substring(1);
+                System.out.println(value);
+            }
+            body += "\"" + key + "\"" + ":\"" + dealNullStr(value) + "\",";
         }
-        System.out.println(new GetsubTaskListReq().toString());
-        //todo
+        body = body.substring(0, body.length() - 1);   //去掉末尾的逗号,
+        return body;
+    }
+
+    private static String dealNullStr(String str) {
+        return StringUtils.isBlank(str) ? "" : str;
     }
 
 }
