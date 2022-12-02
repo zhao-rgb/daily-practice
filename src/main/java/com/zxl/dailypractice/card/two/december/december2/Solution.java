673686746e95013aa297606ec2b9bdb1011a114d
zhao-rgb <1019916061@qq.com>
2022-12-02 09:51:58 +0800

    java基础12.2

---
+++ src/main/java/com/zxl/dailypractice/card/two/december/december2/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.two.december.december2;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/12/2
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
