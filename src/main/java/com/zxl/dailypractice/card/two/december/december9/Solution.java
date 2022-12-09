8f529bc45df3d797e04746675e1431b32eb7c67d
zhao-rgb <1019916061@qq.com>
2022-12-09 14:05:04 +0800

    java基础12.9

---
+++ src/main/java/com/zxl/dailypractice/card/two/december/december9/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.two.december.december9;
+
+/**
+ * @author: zhaoxl
+ * @date: 2022/12/9
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        new Solution();
+    }
+}
