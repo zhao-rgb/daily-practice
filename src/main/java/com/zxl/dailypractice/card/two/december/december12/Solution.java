5372ccadfbe5b1d2df04615338e418acbb3e58d2
zhao-rgb <1019916061@qq.com>
2022-12-13 09:23:05 +0800

    java基础12.13

--- src/main/java/com/zxl/dailypractice/card/two/december/december12/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/two/december/december12/Solution.java
@@ -1,5 +1,8 @@
 package com.zxl.dailypractice.card.two.december.december12;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * @author: zhaoxl
  * @date: 2022/12/9
@@ -8,6 +11,21 @@ package com.zxl.dailypractice.card.two.december.december12;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println("hello");
+        String h = "[{\"CODE\": \"red\",\"VALUE\": true},{\"CODE\": \"blue\",\"VALUE\": true},{\"CODE\": \"hello\",\"VALUE\": true}]";
+        JSONArray hh = JSONArray.parseArray(h);
+        JSONArray objects = solution.addDataToParaArr(hh);
+        System.out.println(objects);
+        for (int i = 0; i < objects.size(); i++) {
+            JSONObject jsonObject = objects.getJSONObject(i);
+            System.out.println(jsonObject);
+        }
+    }
+
+    private JSONArray addDataToParaArr(JSONArray paraArr){
+        JSONArray paraArrFormat = null;
+
+        paraArrFormat = paraArr;
+
+        return paraArrFormat;
     }
 }
