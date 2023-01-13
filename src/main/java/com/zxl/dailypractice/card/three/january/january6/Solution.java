5cd1330522f561d29752c3ef63538fd7c060480f
zhao-rgb <1019916061@qq.com>
2023-01-13 09:17:49 +0800

    java基础23.1.13

--- src/main/java/com/zxl/dailypractice/card/three/january/january6/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/three/january/january6/Solution.java
@@ -1,5 +1,9 @@
 package com.zxl.dailypractice.card.three.january.january6;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Iterator;
+
 /**
  * @author: zhaoxl
  * @date: 2023/1/6
@@ -12,7 +16,21 @@ public class Solution {
 //        jsonObject.put("endTime", "endTime");
 //        String busiParams = JSON.toJSONString(jsonObject);
 //        System.out.println(busiParams);
-        String a = "{\\\"display ip routing-table vpn-instance MCN-PI0 221.238.71.73\\\":\\\"display ip routing-table vpn-instance MCN-PI0 221.238.71.73\\\\r\\\\nRoute Flags: R - relay, D - download to fib, T - to vpn-instance, B - black hole route\\\\r\\\\n------------------------------------------------------------------------------\\\\r\\\\nRouting Table : MCN-PI0\\\\r\\\\nSummary Count : 2\\\\r\\\\n\\\\r\\\\nDestination/Mask    Proto   Pre  Cost        Flags NextHop         Interface\\\\r\\\\n\\\\r\\\\n        0.0.0.0/0   IBGP    255  0             RD  240E:188:9:0:100:0:1004:0 SRv6 BE\\\\t\\\\t\\\\t\\\\r\\\\n                    IBGP    255  0             RD  240E:188:9::1004:0 SRv6 BE\\\\r\\\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>\\\"}";
-        System.out.println(a);
+        String a = "{\n" +
+                "    \"resultId\": \"0001\",\n" +
+                "    \"resultData\": {\n" +
+                "        \"display ip routing-table vpn-instance MCN-PI0 221.238.230.201\\ndisplay ip routing-table vpn-instance MCN-PI0 221.238.230.145\\n\": \"display ip routing-table vpn-instance MCN-PI0 221.238.230.201\\r\\nRoute Flags: R - relay, D - download to fib, T - to vpn-instance, B - black hole route\\r\\n------------------------------------------------------------------------------\\r\\nRouting Table : MCN-PI0\\r\\nSummary Count : 1\\r\\n\\r\\nDestination/Mask    Proto   Pre  Cost        Flags NextHop         Interface\\r\\n\\r\\n221.238.230.201/32  Direct  0    0             D   127.0.0.1       Eth-Trunk11.1283\\t\\t\\t\\r\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>display ip routing-table vpn-instance MCN-PI0 221.238.230.145\\r\\nRoute Flags: R - relay, D - download to fib, T - to vpn-instance, B - black hole route\\r\\n------------------------------------------------------------------------------\\r\\nRouting Table : MCN-PI0\\r\\nSummary Count : 1\\r\\n\\r\\nDestination/Mask    Proto   Pre  Cost        Flags NextHop         Interface\\r\\n\\r\\n221.238.230.145/32  Direct  0    0             D   127.0.0.1       Eth-Trunk11.1283\\t\\t\\t\\r\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>\\r\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>\"\n" +
+                "    },\n" +
+                "    \"resultDesc\": \"{\\\"display ip routing-table vpn-instance MCN-PI0 221.238.230.201\\\\ndisplay ip routing-table vpn-instance MCN-PI0 221.238.230.145\\\\n\\\":\\\"display ip routing-table vpn-instance MCN-PI0 221.238.230.201\\\\r\\\\nRoute Flags: R - relay, D - download to fib, T - to vpn-instance, B - black hole route\\\\r\\\\n------------------------------------------------------------------------------\\\\r\\\\nRouting Table : MCN-PI0\\\\r\\\\nSummary Count : 1\\\\r\\\\n\\\\r\\\\nDestination/Mask    Proto   Pre  Cost        Flags NextHop         Interface\\\\r\\\\n\\\\r\\\\n221.238.230.201/32  Direct  0    0             D   127.0.0.1       Eth-Trunk11.1283\\\\t\\\\t\\\\t\\\\r\\\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>display ip routing-table vpn-instance MCN-PI0 221.238.230.145\\\\r\\\\nRoute Flags: R - relay, D - download to fib, T - to vpn-instance, B - black hole route\\\\r\\\\n------------------------------------------------------------------------------\\\\r\\\\nRouting Table : MCN-PI0\\\\r\\\\nSummary Count : 1\\\\r\\\\n\\\\r\\\\nDestination/Mask    Proto   Pre  Cost        Flags NextHop         Interface\\\\r\\\\n\\\\r\\\\n221.238.230.145/32  Direct  0    0             D   127.0.0.1       Eth-Trunk11.1283\\\\t\\\\t\\\\t\\\\r\\\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>\\\\r\\\\n<TJ-DL-XSW2-B-6.STN.CX600-X8A>\\\"}\"\n" +
+                "}";
+        JSONObject jsonObject = JSONObject.parseObject(a);
+        System.out.println(jsonObject);
+        JSONObject resultData = jsonObject.getJSONObject("resultData");
+        Iterator<String> iterator = resultData.keySet().iterator();
+        while (iterator.hasNext()){
+            String key = iterator.next();
+            String value = resultData.getString(key);
+            System.out.println(value);
+        }
     }
 }
--- src/main/java/com/zxl/dailypractice/card/three/january/january9/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/three/january/january9/Solution.java
@@ -23,5 +23,25 @@ public class Solution {
         String s = Integer.toString(taskid);
         System.out.println(s);
         System.out.println("hello");
+
+//        //割接状态更改
+//        GetsubTaskListReq getsubTaskListReq = new GetsubTaskListReq();
+//        getsubTaskListReq.setTaskid(req.getTaskid());
+//        RestPageRequest.Page pageReq = new RestPageRequest.Page();
+//        pageReq.setPageNum(1);
+//        pageReq.setPageSize(500);
+//        getsubTaskListReq.setPage(pageReq);
+//        Map<String, Object> map = getsubTaskList(getsubTaskListReq);
+//        logger.info(map.get("records").toString());
+//
+////        List<GetsubTaskListResp> getsubTaskListResps = new ArrayList<>();
+////        if (map.get("records") )
+//        JSONArray records = JSONArray.parseArray(map.get("records").toString());
+//        for (Object a : records){
+//            JSONObject jsonObject = JSONObject.parseObject(a.toString());
+//            String newexecutestatus = jsonObject.getString("newexecutestatus");
+//            logger.info(newexecutestatus);
+//        }
+//        return RestResponse.ok();
     }
 }
