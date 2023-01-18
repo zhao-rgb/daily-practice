a1cd0ed29104020e02b74120336f55124aea1f4e
zhao-rgb <1019916061@qq.com>
2023-01-18 09:30:47 +0800

    java基础23.1.18

--- src/main/java/com/zxl/dailypractice/project/controller/BizController.java
+++ src/main/java/com/zxl/dailypractice/project/controller/BizController.java
@@ -12,16 +12,20 @@ import com.zxl.dailypractice.project.util.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -88,9 +92,9 @@ public class BizController {
         return ResponseResult.success();
     }
 
-    @ApiOperation("download")
-    @PostMapping ("/download")
-    void download(HttpServletRequest req, HttpServletResponse resp) {
+    @ApiOperation("readdownload")
+    @PostMapping ("/read/download")
+    void readdownload(HttpServletRequest req, HttpServletResponse resp) {
         File file2=new File("E:\\Unitehcs\\data.csv");
         //获取文件名
         String fileName="hello";
@@ -130,5 +134,75 @@ public class BizController {
                 log.info("resp返回前端信息异常");
             }
         }
+    }
+
+    @ApiOperation("download")
+    @PostMapping ("/download")
+    void download(HttpServletRequest req, HttpServletResponse resp){
+        File file=new File("E:\\Unitehcs\\ZJ_ipranPortInfo_all_20221212040230.json.gz");
+        downloadZip(file,resp);
+    }
+
+    @ApiOperation("downloadz")
+    @PostMapping ("/downloadz")
+    public ResponseEntity<byte[]> downloadz(HttpServletRequest req, HttpServletResponse resp){
+        File file=new File("E:\\Unitehcs\\workfile\\12月\\30518\\ZJ_ipranPortInfo_all_20221212040230.json.gz");
+        byte[] fileData = downloadFile(file);
+        String filename = "XZ_ipranDeviceInfo_all_20230116081405.json.gz";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment;filename=" + filename);
+        headers.setContentLength(fileData.length);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> entity = new ResponseEntity<>(fileData, headers, statusCode);
+        return entity;
+    }
+
+    public byte[] downloadFile(File file) {
+        InputStream inputStream = null;
+        try {
+            inputStream = new BufferedInputStream(new FileInputStream(file));;
+            ByteArrayOutputStream  byteOutputStream = new ByteArrayOutputStream();
+            IOUtils.copy(inputStream, byteOutputStream);
+            return byteOutputStream.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("下载文件异常", e);
+        } finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    log.error("关闭下载文件输入流异常", e);
+                }
+            }
+        }
+    }
+
+    public static HttpServletResponse downloadZip(File file, HttpServletResponse response) {
+        try {
+            // 以流的形式下载文件。
+            InputStream fis = new BufferedInputStream(new FileInputStream(file.getPath()));
+            // 清空response
+            response.reset();
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            // 如果输出的是中文名的文件，在此处就要用URLEncoder.encode方法进行处理
+            response.setHeader("Content-Disposition",
+                    "attachment;filename=" + URLEncoder.encode(file.getName(), "UTF-8"));
+
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = fis.read(buffer)) != -1) {
+                toClient.write(buffer, 0, len);
+                toClient.flush();
+            }
+            fis.close();
+            toClient.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+
+        }
+        return response;
     }
 }
