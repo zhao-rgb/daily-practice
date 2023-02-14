bcf0645cc00af9fa4820ef93b61ad461a8a12143
zhao-rgb <1019916061@qq.com>
2023-02-14 09:41:59 +0800

    java基础23.2.14

--- src/main/java/com/zxl/dailypractice/card/three/february/february7/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/three/february/february7/Solution.java
@@ -1,5 +1,13 @@
 package com.zxl.dailypractice.card.three.february.february7;
 
+import com.zxl.dailypractice.project.controller.req.GetsubTaskListReq;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * @author: zhaoxl
  * @date: 2023/2/7
@@ -7,39 +15,39 @@ package com.zxl.dailypractice.card.three.february.february7;
  **/
 public class Solution {
     public static void main(String[] args) throws IllegalAccessException {
-//        //去除数字
-//        String REGEX = "[^0-9]";
-//        String ticketStr = "c11";
-//        String ticket = Pattern.compile(REGEX).matcher(ticketStr).replaceAll("").trim();
-//        System.out.println(ticket);
-//        String a = "display current-configuration\\r\\n!Software Version V800R011C10SPC800\\r\\n";
-//        System.out.println(a);
-//        //注意replace和replaceall区别
-//        String fileContent = a.replace("\\r\\n",System.getProperty("line.separator"));
-//        System.out.println(fileContent);
-//
+        //去除数字
+        String REGEX = "[^0-9]";
+        String ticketStr = "c11";
+        String ticket = Pattern.compile(REGEX).matcher(ticketStr).replaceAll("").trim();
+        System.out.println(ticket);
+        String a = "display current-configuration\\r\\n!Software Version V800R011C10SPC800\\r\\n";
+        System.out.println(a);
+        //注意replace和replaceall区别
+        String fileContent = a.replace("\\r\\n",System.getProperty("line.separator"));
+        System.out.println(fileContent);
+
         int taskid = 744224;
         String s = String.valueOf(taskid);
         System.out.println(s);
 
-//        List<GetsubTaskListReq> list = new ArrayList<>();
-//
-//        GetsubTaskListReq getsubTaskListReq1 = new GetsubTaskListReq();
-//        getsubTaskListReq1.setAccessnum("12s2");
-//        GetsubTaskListReq getsubTaskListReq2 = new GetsubTaskListReq();
-//        getsubTaskListReq2.setAccessnum("s2jis2hs2");
-//        list.add(getsubTaskListReq1);
-//        list.add(getsubTaskListReq2);
-//
-//        for (int i = 0; i < list.size(); i++) {
-//            Field[] fields = list.get(i).getClass().getDeclaredFields();
-//            System.out.println(Arrays.toString(fields));
-//            for (int j = 0; j < fields.length; j++) {
-//                fields[j].setAccessible(true);
-//                //获取字段名称
-//                String value = fields[j].get(list.get(i)).toString();
-//                System.out.println(value);
-//            }
-//        }
+        List<GetsubTaskListReq> list = new ArrayList<>();
+
+        GetsubTaskListReq getsubTaskListReq1 = new GetsubTaskListReq();
+        getsubTaskListReq1.setAccessnum("12s2");
+        GetsubTaskListReq getsubTaskListReq2 = new GetsubTaskListReq();
+        getsubTaskListReq2.setAccessnum("s2jis2hs2");
+        list.add(getsubTaskListReq1);
+        list.add(getsubTaskListReq2);
+
+        for (int i = 0; i < list.size(); i++) {
+            Field[] fields = list.get(i).getClass().getDeclaredFields();
+            System.out.println(Arrays.toString(fields));
+            for (int j = 0; j < fields.length; j++) {
+                fields[j].setAccessible(true);
+                //获取字段名称
+                String value = fields[j].get(list.get(i)).toString();
+                System.out.println(value);
+            }
+        }
     }
 }
