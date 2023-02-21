d87ef11ca48042dad0ef439d4bb6e2f221ea584c
zhao-rgb <1019916061@qq.com>
2023-02-21 09:35:50 +0800

    java基础23.2.21

--- src/main/java/com/zxl/dailypractice/project/controller/EsController.java
+++ src/main/java/com/zxl/dailypractice/project/controller/EsController.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.unitechs.framework.elasticsearch.client.EsTemplate;
 import com.unitechs.framework.elasticsearch.domain.request.QueryDocumentByDslRequest;
+import com.unitechs.framework.elasticsearch.domain.request.QueryDocumentRequest;
 import com.unitechs.framework.elasticsearch.domain.response.QueryDocumentResponse;
 import com.zxl.dailypractice.project.util.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author: zhaoxl
  * @date: 2022/10/14
  * @desc:
  **/
+@Slf4j
 @RestController
 @Api(tags = "熟悉ES")
 public class EsController {
@@ -49,6 +54,37 @@ public class EsController {
             System.out.println(jsonArray.getJSONObject(i).getJSONObject("_source").get("d_memr"));
         }
         return ResponseResult.success(jsonObjectQueryDocumentResponse);
+    }
+
+    @ApiOperation("ES测试")
+    @RequestMapping(value = "/test/es",method = RequestMethod.POST)
+    public ResponseResult testes() throws Exception{
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String now = time.format(formatter);
+        String indexName = "pm_col_brasippool" + now;
+        String sql = "select * from " + indexName + " limit 10000";
+        log.info("===============sql语句为：" + sql);
+
+        indexName = "am_ala_alarmsummary_20230217";
+        log.info("esSearchIndex:{}",indexName);
+        QueryDocumentRequest<JSONObject> queryDocumentRequest = new QueryDocumentRequest<JSONObject>(indexName,"_doc",sql,JSONObject.class);
+        QueryDocumentResponse<JSONObject> jsonObjectQueryDocumentResponse = esTemplate.queryDocument(queryDocumentRequest);
+        String data = jsonObjectQueryDocumentResponse.getJsonString();
+        log.info("====================data:" + data);
+        JSONObject object = JSONObject.parseObject(data);
+        JSONObject hits = (JSONObject) object.get("hits");
+        JSONArray array = hits.getJSONArray("hits");
+        if (array != null && array.size() > 0){
+            for (int i = 0;i < array.size();i++){
+                JSONObject source = ((JSONObject) array.get(i)).getJSONObject("_source");
+                if (!source.isEmpty()){
+                    String v4T = source.getString("k_ALARMCLASS");
+                    System.out.println(v4T);
+                }
+            }
+        }
+        return ResponseResult.success(data);
     }
 
     @Bean
