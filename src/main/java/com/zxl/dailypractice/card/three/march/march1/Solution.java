0b1e7629527330a5a673d14143eb941c9006b2ad
zhao-rgb <1019916061@qq.com>
2023-03-02 09:48:04 +0800

    java基础23.3.2

---
+++ src/main/java/com/zxl/dailypractice/card/three/march/march1/Solution.java
@@ -0,0 +1,14 @@
+package com.zxl.dailypractice.card.three.march.march1;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/3/1
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        String d_iPv4PoolTotal = "2752";
+        String d_IPv4PoolUseRatio = "25";
+        System.out.println(String.valueOf(Integer.parseInt(d_iPv4PoolTotal)*Integer.parseInt(d_IPv4PoolUseRatio)/100));;
+    }
+}
