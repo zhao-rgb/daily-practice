668290ef0488b4e12501e7aaad7b9fb1619d71f7
zhao-rgb <1019916061@qq.com>
2023-03-12 16:46:56 +0800

    java基础23.3.12

---
+++ src/main/java/com/zxl/dailypractice/card/three/march/march11/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.three.march.march11;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/3/11
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("找工作好难");
+    }
+}
