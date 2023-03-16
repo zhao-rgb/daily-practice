256688cc27fc10723152ab8c2b8f97def5d39819
zhao-rgb <1019916061@qq.com>
2023-03-16 09:53:42 +0800

    java基础23.3.16

---
+++ src/main/java/com/zxl/dailypractice/card/three/march/march16/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.three.march.march16;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/3/16
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        new Solution();
+    }
+}
