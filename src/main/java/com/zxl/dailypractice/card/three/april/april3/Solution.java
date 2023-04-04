41e8ed5f2f238c008b1ec8b81da8b255354fc57c
zhao-rgb <1019916061@qq.com>
2023-04-04 09:30:25 +0800

    java基础23.4.4

---
+++ src/main/java/com/zxl/dailypractice/card/three/april/april3/Solution.java
@@ -0,0 +1,15 @@
+package com.zxl.dailypractice.card.three.april.april3;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/4/3
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        String a = "hhh";
+        for (int i = 0; i < 10; i++) {
+            System.out.print("<" + a + ">,<" + a + ">异常\r\n");
+        }
+    }
+}
