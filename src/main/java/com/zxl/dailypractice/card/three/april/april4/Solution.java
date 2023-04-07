09225c74c41a03c43ade891ca809e64e5b2be841
zhao-rgb <1019916061@qq.com>
2023-04-07 09:35:27 +0800

    java基础23.4.7

--- src/main/java/com/zxl/dailypractice/card/three/april/april4/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/three/april/april4/Solution.java
@@ -1,7 +1,8 @@
 package com.zxl.dailypractice.card.three.april.april4;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * @author: zhaoxl
@@ -9,7 +10,7 @@ import java.util.regex.Pattern;
  * @desc:
  **/
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 //        String checkValue = "70";
 //        String checkExpr = "80";
 //        String checkResult = "0";
@@ -31,11 +32,20 @@ public class Solution {
 //                }
 //                break;
 //        }
-        String checkValue = "70a";
-        String checkExpr = "80";
-        String resultExpr = "(\\d*)";
-        Pattern p = Pattern.compile(resultExpr);
-        Matcher matcher = p.matcher(checkValue);
-        System.out.println(matcher.replaceAll(""));
+//        String checkValue = "70a";
+//        String checkExpr = "80";
+//        String resultExpr = "(\\d*)";
+//        Pattern p = Pattern.compile(resultExpr);
+//        Matcher matcher = p.matcher(checkValue);
+//        System.out.println(matcher.replaceAll(""));
+
+        String fileName = "test - 副本,.txt";
+        String newFileName = new String(fileName.getBytes(), "utf-8");
+        String replace = fileName.replace(",", "");
+        System.out.println(String.format("attachment; filename=\"%s\"", replace));
+
+        if(NumberUtils.isDigits("2222")){
+            System.out.println("hhh");
+        }
     }
 }
