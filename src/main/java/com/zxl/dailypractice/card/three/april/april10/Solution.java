59c7c0b21582b926636416ce3e0faa84a42e04d5
zhao-rgb <1019916061@qq.com>
2023-04-10 13:53:24 +0800

    java基础23.4.10

---
+++ src/main/java/com/zxl/dailypractice/card/three/april/april10/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.three.april.april10;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/4/10
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
