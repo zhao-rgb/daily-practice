ab395a91360ed29ff6387cb1fa5055b277c2172c
zhao-rgb <1019916061@qq.com>
2023-04-13 09:24:17 +0800

    java基础23.4.13

--- src/main/java/com/zxl/dailypractice/card/three/april/april10/Solution.java
+++ src/main/java/com/zxl/dailypractice/card/three/april/april10/Solution.java
@@ -7,6 +7,23 @@ package com.zxl.dailypractice.card.three.april.april10;
  **/
 public class Solution {
     public static void main(String[] args) {
+//        LocalDateTime startTime = LocalDateTime.now();
+//        // some time-consuming operation here
+//        for (int i = 0; i < 1000; i++) {
+//            System.out.println(i);
+//        }
+//        LocalDateTime endTime = LocalDateTime.now();
+//        Duration duration = Duration.between(startTime, endTime);
+//        System.out.println("Time difference: " + duration.toMillis() + " milliseconds");
 
+
+        long startTime = System.currentTimeMillis();
+        // code for event 1
+        for (int i = 0; i < 500; i++) {
+            System.out.println(i);
+        }
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Time elapsed for event 1: " + timeElapsed + " milliseconds");
     }
 }
