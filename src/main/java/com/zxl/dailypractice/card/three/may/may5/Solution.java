5d141e90683e03faaa1f619f5f71d6cd752cd62e
zhao-rgb <1019916061@qq.com>
2023-05-06 09:15:40 +0800

    java基础23.5.6

---
+++ src/main/java/com/zxl/dailypractice/card/three/may/may5/Solution.java
@@ -0,0 +1,12 @@
+package com.zxl.dailypractice.card.three.may.may5;
+
+/**
+ * @author: zhaoxl
+ * @date: 2023/5/4
+ * @desc:
+ **/
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
